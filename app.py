7045067142b8702ab79acddccd73924a4b09dcf7
Tushar Kanti Jaiswal <karanwalia2001@gmail.com>
2021-04-24 22:03:46 +0530

    change infinity

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from predict import predict
 from explore import explore
 from text_p import text_processing
 import nltk
-nltk.data.path.append('/app/.heroku/python/nltk_data')
+# nltk.data.path.append('/app/.heroku/python/nltk_data')
 
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Explore", "Predict"))
